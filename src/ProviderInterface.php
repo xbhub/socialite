74524e06c1cfabe53fc63472a4625e88a11f96bf
jory <jorycn@163.com>
2019-08-30 09:30:16 +0800

    init

---
+++ src/ProviderInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the overtrue/socialite.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Packages\Socialite;
+
+interface ProviderInterface
+{
+    /**
+     * Redirect the user to the authentication page for the provider.
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function redirect();
+
+    /**
+     * Get the User instance for the authenticated user.
+     *
+     * @param \Packages\Socialite\AccessTokenInterface $token
+     *
+     * @return \Packages\Socialite\User
+     */
+    public function user(AccessTokenInterface $token = null);
+}
